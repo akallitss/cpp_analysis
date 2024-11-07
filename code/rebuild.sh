8407349362f775d3211dab41271fa926faf88fae
akallitss <alexandra.kallitsopoulou@cern.ch>
2024-11-07 19:29:12 +0100

    added the exit of root

--- code/rebuild.sh
+++ code/rebuild.sh
@@ -1,3 +1,4 @@
 rm -r *pcm *.so *d
 root -l -b -q MyFunctions.C+
-root -l 'AnalyseTreePicosec.C++(224, 2, 1)'
+root -l 'AnalyseTreePicosec.C++(224, 2)'
+#root -l -b -q 'AnalyseTreePicosec.C++(224, 2)'
