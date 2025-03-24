105803587e50adff6c182ee46b85b19fae343974
akallits <alexandra.kallitsopoulou@cern.ch>
2025-03-24 21:33:50 +0100

    rebuild.sh to run faster for debugging

---
+++ code/2023_April_h4/rebuild.sh
@@ -0,0 +1,6 @@
+rm -r *pcm *.so *d
+root -l -b -q MyFunctions_2023_April.C++
+#root -l 'AnalyseTreePicosec_2023_April.C++(243, 2)'
+#root -l 'AnalyseTreePicosec_2023_April.C+(243, 2, 1)'
+#root -l -b 'AnalyseTreePicosec_2023_April.C+(243, 2, 1)'
+root -l -b -q 'AnalyseTreePicosec_2023_April.C++(243, 4)'
