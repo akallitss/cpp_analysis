95e41f2be331b9c9bcaa52236ec0f48712816e70
akallits <alexandra.kallitsopoulou@cern.ch>
2025-05-01 19:11:53 +0200

    change the code name

--- code/2024_June_h4/condor_submit/submit_to_condor_make_tree.sh
+++ code/2024_June_h4/condor_submit/submit_to_condor_make_tree.sh
@@ -5,6 +5,6 @@ rm -r *pcm *.so *d *_ACLiC_*
 rm err_make_tree/*
 rm out_make_tree/*
 rm log_make_tree/*
-root -l -b -q "../MyFunctions_2023_June.C++"
-root -l -b -q "../MakeTreefromRawTreePicosecJune23.C++"
+root -l -b -q "../MyFunctions_2024_June.C++"
+root -l -b -q "../MakeTreefromRawTreePicosecJune24.C++"
 condor_submit job_make_tree.submit
